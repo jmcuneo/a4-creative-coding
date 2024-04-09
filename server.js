75355dd4fbc44e17a379d2d34c52acb465d4f2f4
lfoley7 <luke.c.foley@gmail.com>
2024-04-09 17:16:31 -0400

    start on index.html

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 let gameSettings = {
     width: 5,
