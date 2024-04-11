46a80f33c2b047c29b71808b15296b93381b7bb4
Ronak Wani <ronak_wani@outlook.com>
2024-04-10 21:53:55 -0400

    MODULE not found error

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const dotenv = require("dotenv").config();
+
+// Define the port
+const port = process.env.PORT || 3000; // Use process.env.PORT or default to 3000 if not specified in the environment variables
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
