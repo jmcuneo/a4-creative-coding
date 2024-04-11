6bc2e0f1f067cbe76e87708feb8447ae516837aa
jhokk <jayhokkanen25@gmail.com>
2024-04-11 17:57:21 -0400

    gain

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,8 @@
 import { Pane } from "tweakpane";
 
 const PARAMS = {
-  factor: 123,
+  gain: 1.0,
   color: { r: 255, g: 0, b: 55 },
-};
-
-// Creates audio context, audio element, analyser
-// Plays audio and returns analyser
-const setupAudio = function () {
-  const audioCtx = new AudioContext();
-
-  const audioElement = document.createElement("audio");
-  document.body.appendChild(audioElement);
-
-  const analyser = audioCtx.createAnalyser();
-  analyser.fftSize = 2048;
-
-  const player = audioCtx.createMediaElementSource(audioElement);
-  player.connect(audioCtx.destination);
-  player.connect(analyser);
-
-  audioElement.src = "./test-audio.wav";
-  audioElement.play();
-
-  return analyser;
 };
 
 // Clears the canvas and returns the context
@@ -37,7 +16,24 @@ const clearCanvas = function () {
 };
 
 const play = function () {
-  const analyser = setupAudio();
+  document.querySelector("button").onclick = null;
+
+  const audioCtx = new AudioContext();
+
+  const audioElement = document.createElement("audio");
+  document.body.appendChild(audioElement);
+
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+
+  const gainNode = audioCtx.createGain();
+
+  const player = audioCtx.createMediaElementSource(audioElement);
+  player.connect(gainNode).connect(audioCtx.destination);
+  player.connect(analyser);
+
+  audioElement.src = "./test-audio.wav";
+  audioElement.play();
 
   const results = new Uint8Array(analyser.frequencyBinCount);
 
@@ -45,8 +41,8 @@ const play = function () {
     window.requestAnimationFrame(draw);
 
     const ctx = clearCanvas();
-    ctx.fillStyle = "white";
     ctx.fillStyle = `rgb(${PARAMS.color.r}, ${PARAMS.color.g}, ${PARAMS.color.b})`;
+    gainNode.gain.value = PARAMS.gain;
     analyser.getByteFrequencyData(results);
 
     for (let i = 0; i < analyser.frequencyBinCount; i++) {
@@ -67,7 +63,7 @@ window.onload = async function () {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   const pane = new Pane({ title: "Controls" });
-  pane.addBinding(PARAMS, "factor");
+  pane.addBinding(PARAMS, "gain", { min: 0.25, max: 5.0 });
   pane.addBinding(PARAMS, "color");
 
   document.querySelector("button").onclick = play;
