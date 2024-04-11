df2add0f1b8c4e9cbada156a11fa8a4a49708941
jhokk <jayhokkanen25@gmail.com>
2024-04-11 16:53:36 -0400

    b&w visualization for test audio

---
+++ public/js/main.js
@@ -0,0 +1,50 @@
+import { Pane } from "tweakpane";
+
+const play = function () {
+  const audioCtx = new AudioContext();
+
+  const audioElement = document.createElement("audio");
+  document.body.appendChild(audioElement);
+
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 1024;
+
+  const player = audioCtx.createMediaElementSource(audioElement);
+  player.connect(audioCtx.destination);
+  player.connect(analyser);
+
+  audioElement.src = "./test-audio.wav";
+  audioElement.play();
+
+  const results = new Uint8Array(analyser.frequencyBinCount);
+
+  const draw = function () {
+    window.requestAnimationFrame(draw);
+
+    const canvas = document.querySelector("canvas");
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "white";
+    analyser.getByteFrequencyData(results);
+
+    for (let i = 0; i < analyser.frequencyBinCount; i++) {
+      ctx.fillRect(i, 512, 1, -1 * results[i]);
+    }
+  };
+  draw();
+};
+
+window.onload = async function () {
+  const canvas = document.createElement("canvas");
+  document.body.appendChild(canvas);
+
+  canvas.width = canvas.height = 512;
+  const ctx = canvas.getContext("2d");
+
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  document.querySelector("button").onclick = play;
+};
